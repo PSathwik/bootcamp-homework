790b3a90e22068c3a0adc704f2b296e3766a5b4e
PSathwik <sathwikp@advisory.com>
2016-02-05 00:58:04 +0530

    Displaying stock API content using handlebars

---
+++ assets/app/js/stocks.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('selectr').selectr({
+		filter: false,
+		footer: false
+	});
+	
+	$('#stockInput').on('keypress', function(key) {
+		if(key.keyCode === 13) {
+			$('.primary').click();
+		}
+	});
+	
+	$('.primary').on('click', function(e){
+		var loopIndex = 0;
+		var stock = $('#stockInput').val();
+		
+		var source = $('#data-row-template').html();
+		var template = Handlebars.compile(source);
+		
+		$.ajax({
+			url:'https://stockvider.p.mashape.com/indicator/DATA/' + stock + '/?end_date=2015-07-20&start_date=2015-05-20',
+			crossDomain: true,
+			headers: {
+				'X-Mashape-Key': 'Bs5BvTwMeNmshIVgyxatfWRfPMkNp1Dmi30jsnLUNZ8zyDyBW8'
+			},
+			success: function(data){
+				if (data.Error) {
+					alert(data.Error);
+				}
+				
+				var source = $('#data-row-template').html();
+				var template = Handlebars.compile(source);
+				
+				var dataSet = data.Dataset;
+				var parent = $('table');
+
+				for(loopIndex = 0; loopIndex < dataSet.length; ++loopIndex) {
+					parent.append(template(dataSet[loopIndex]));
+				}
+				
+				data.Dataset
+			}
+		});
+	})
+});
